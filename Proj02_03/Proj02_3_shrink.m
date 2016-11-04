18846a3cd544a2f028ad40eab0e9d38a0e9ffc35
Washington Pinto Lisboa <wlisboa.hc@gmail.com>
2016-11-04 11:22:40 -0400

    inclusão de arquivos
    Inclusão do relatório e do arquivo de shrink

---
+++ Proj02_03/Proj02_3_shrink.m
@@ -0,0 +1,36 @@
+% Proj02_03
+% Autor: Washington Pinto Lisboa
+% Redução das dimensões da imagem através da eliminação de linhas e colunas
+
+clc;
+clear all;
+close all;
+
+% Caminho do arquivo que deverá ser modificado
+path = '/Users/wlisboa/Documents/Documentos VBox /Mestrado/PDI/Trabalhos/Proj02-03/imagens/Fig0219(rose1024).tif';
+
+I = imread(path);     % Imagem de entrada 
+
+% Fator de redução
+f=input('Entre com o fator de redução da imagem: ');
+
+[M N]=size(I)
+
+M1 = M/f
+N1 = N/f
+
+k=1;
+l=1;
+for i=1:M1
+    for j=1:N1
+        I_out(i,j)=I(k,l);
+        l=l+f;
+    end
+    l=1;
+    k=k+f;
+end
+figure,imshow(I)
+title('Imagem Original');
+figure,imshow(I_out)
+title('Imagem Reduzida');
+%imwrite(I_out,'Fig0219(shr32).tif');
